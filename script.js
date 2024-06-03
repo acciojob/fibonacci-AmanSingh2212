99f5be68f01ab72576bcf0439f34eb253a34d640
Aman Singh <112865635+AmanSingh2212@users.noreply.github.com>
2024-06-03 20:20:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,28 @@
 function fibonacci(num) {
-// your code here
+
+    int num1 = 0;
+	int num2 = 1;
+
+	if(num == 1)
+	{
+		return 0;
+	}
+	if(num == 2)
+	{
+		return 1;
+	}
+    let ans = 0;
+	for(let i=3; i<=num; i++)
+		{
+			ans = num1 + num2 ;
+			
+			// let temp = num1;
+			num1 = num2;
+			num2 = ans;
+		}
+
+	return ans;
+	
 }
 
 module.exports = fibonacci;
